9c262b4af5d855b0651e2c5617712e5e40ef41ec
manousos <man.mathioudakis@gmail.com>
2018-12-07 02:06:37 +0200

    alter repositories

--- src/main/java/gr/manousos/product/repository/CustomerOrderRepository.java
+++ src/main/java/gr/manousos/product/repository/CustomerOrderRepository.java
@@ -1,11 +1,25 @@
 package gr.manousos.product.repository;
 
 import gr.manousos.product.domain.CustomerOrder;
+import gr.manousos.product.domain.projection.OrderDetailsProjection;
+import java.time.Instant;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Repository for {@link CustomerOrder} entity.
  */
 public interface CustomerOrderRepository extends JpaRepository<CustomerOrder, Long> {
 
+
+  /**
+   * Find all orders within a given time period.
+   *
+   * @param dateFrom the date from (placed orders).
+   * @param dateTo the date until (placed orders).
+   * @return list of all orders within a given time period.
+   */
+  @Query("select co from CustomerOrder co join fetch co.productOrders po join fetch po.product where co.placedDate between ?1 and ?2")
+  List<OrderDetailsProjection> getAllByByPlacedDateBetween(Instant dateFrom, Instant dateTo);
 }
--- src/main/java/gr/manousos/product/repository/ProductOrderRepository.java
+++ src/main/java/gr/manousos/product/repository/ProductOrderRepository.java
@@ -3,10 +3,13 @@ package gr.manousos.product.repository;
 import gr.manousos.product.domain.ProductOrder;
 import gr.manousos.product.domain.ProductOrderPK;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * Repository for {@link ProductOrder} entity.
  */
 public interface ProductOrderRepository extends JpaRepository<ProductOrder, ProductOrderPK> {
 
+  @Query("select sum (po.productPrice) from ProductOrder po where po.productOrderPK.customerOrderId= :orderId ")
+  Double getPriceSumbyOrderId(Long orderId);
 }
